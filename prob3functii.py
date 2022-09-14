44bed145481189c7487a502039a87036f11d0498
SobuleacCatalina <71014246+SobuleacCatalina@users.noreply.github.com>
2022-09-14 11:46:26 +0300

    Update and rename prob1.py to prob3functii.py

--- prob3functii.py
+++ prob3functii.py
@@ -4,4 +4,9 @@ def adunare(x,y,n,m):
     i=(x*m)+(n*y)
     j=y*m
     return (i,j)
+def inmultire(x,y,n,m):
+    i=x*n
+    j=y*m
+    return i,j
 print(a,'/',b,'+',c,'/',d,'=',adunare(a,b,c,d))
+print(a,'/',b,'*',c,'/',d,'=',inmultire(a,b,c,d))
